2ecef40987386277187594256223dabf8322412e
Mateusz Czarczyński <mczarczynski@edu.cdv.pl>
2021-03-05 19:01:36 +0100

    cleaning up

---
+++ Lab1_czarczynski/Lab1_czarczynski/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Lab1_czarczynski.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
---
+++ Lab1_czarczynski/Lab1_czarczynski/Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Lab1_czarczynski.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1_czarczynski.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Person> People{ get; set; }
+    }
+}
