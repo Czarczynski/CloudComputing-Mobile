5727ac34cf8d9c0494fb4a0d10a18649362913d0
Mateusz Czarczyński <mczarczynski@edu.cdv.pl>
2021-03-05 19:04:06 +0100

    peoplecontroller created

---
+++ Lab1_czarczynski/Lab1_czarczynski/Controllers/PeopleController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1_czarczynski.Controllers
+{
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+    }
+}
