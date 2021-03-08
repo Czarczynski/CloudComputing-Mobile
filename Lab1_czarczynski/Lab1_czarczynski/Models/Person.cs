4c08be73f6d03b87ff389c3494bd9e937f28f985
Mateusz Czarczyński <mczarczynski@edu.cdv.pl>
2021-03-08 12:58:40 +0100

    Dto model created

--- Lab1_czarczynski/Lab1_czarczynski/Models/Person.cs
+++ Lab1_czarczynski/Lab1_czarczynski/Models/Person.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 namespace Lab1_czarczynski.Models
 {
-    public class Person
+
+    public class PersonDTO
     {
-        public int PersonId { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+    }
+    public class Person : PersonDTO
+    {
+        public int PersonId { get; set; }
     }
 }
