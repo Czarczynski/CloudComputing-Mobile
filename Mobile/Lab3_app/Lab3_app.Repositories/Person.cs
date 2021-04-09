70948e8076fba8a1dfd7265bf88199eeec737e13
Mateusz Czarczyński <mczarczynski@edu.cdv.pl>
2021-04-09 19:23:45 +0200

    task 3 done

--- Mobile/Lab3_app/Lab3_app.Repositories/Person.cs
+++ Mobile/Lab3_app/Lab3_app.Repositories/Person.cs
@@ -4,9 +4,9 @@ namespace Lab3_app.Repositories
 {
     public class Person
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public int PersonId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string PhoneNumber { get; set; }
         public string PictureBase64 { get; set; }
     }
